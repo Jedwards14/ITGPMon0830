00bbd54741fb445de07bfcc18d9c57c69e4a2f8e
Jedwards14 <164707246+Jedwards14@users.noreply.github.com>
2025-03-25 14:22:14 +1100

    Player Movement
    Added player movement and camera

---
+++ intro-to-game-production/Jack/character_body_3d.gd
@@ -0,0 +1,45 @@
+extends CharacterBody3D
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+@export var sensivity = 0.3
+
+var target_velocity = Vector3.ZERO
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		$Camera.rotation_degrees.x -= event.relative.y * sensivity
+		$Camera.rotation_degrees.x = clamp($Camera.rotation_degrees.x, -90, 90)
+		rotation_degrees.y -= event.relative.x * sensivity
+		
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_backward"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.basis = Basis.looking_at(direction)
+
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
