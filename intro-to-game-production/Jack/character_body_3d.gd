cb62bdc84c418e0fd1ba06fcb30c0de784b1c3de
Jedwards14 <164707246+Jedwards14@users.noreply.github.com>
2025-03-31 22:05:35 +1100

    Jump and Run
    Added ability to jump and run, changed environment to template

--- intro-to-game-production/Jack/character_body_3d.gd
+++ intro-to-game-production/Jack/character_body_3d.gd
@@ -1,10 +1,10 @@
 extends CharacterBody3D
 # How fast the player moves in meters per second.
-@export var speed = 14
+@export var speed = 2
 # The downward acceleration when in the air, in meters per second squared.
-@export var fall_acceleration = 75
-@export var sensivity = 0.3
- 
+@export var fall_acceleration = 50
+@export var sensivity = 0.1
+@export var jump_impulse = 15
 var target_velocity = Vector3.ZERO
 
 func _ready():
@@ -27,7 +27,18 @@ func _physics_process(delta):
 		direction += transform.basis.z
 	if Input.is_action_pressed("move_forward"):
 		direction -= transform.basis.z
+		
+	if Input.is_action_pressed("respawn"):
+		position = Vector3(0.5,-3.5,10)
 
+	if Input.is_action_just_pressed("run"):
+		speed += 2
+	if Input.is_action_just_released("run"):
+		speed -= 2
+		
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.basis = Basis.looking_at(direction)
@@ -35,7 +46,7 @@ func _physics_process(delta):
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
-
+	
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
