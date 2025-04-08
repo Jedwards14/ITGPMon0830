15d169b9a331f2b1a2b554181dae6ca55dd60c0e
Jedwards14 <164707246+Jedwards14@users.noreply.github.com>
2025-04-08 23:15:46 +1000

    Controller Support
    Added controller support for camera and movement

--- intro-to-game-production/Jack/character_body_3d.gd
+++ intro-to-game-production/Jack/character_body_3d.gd
@@ -3,31 +3,29 @@ extends CharacterBody3D
 @export var speed = 2
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 50
-@export var sensivity = 0.1
+@export var sensitivity = 0.1
 @export var jump_impulse = 15
 var target_velocity = Vector3.ZERO
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-func _input(event):
-	if event is InputEventMouseMotion:
-		$Camera.rotation_degrees.x -= event.relative.y * sensivity
-		$Camera.rotation_degrees.x = clamp($Camera.rotation_degrees.x, -90, 90)
-		rotation_degrees.y -= event.relative.x * sensivity
-		
-func _physics_process(delta):
-	var direction = Vector3.ZERO
 
-	if Input.is_action_pressed("move_right"):
-		direction += transform.basis.x
-	if Input.is_action_pressed("move_left"):
-		direction -= transform.basis.x
-	if Input.is_action_pressed("move_backward"):
-		direction += transform.basis.z
-	if Input.is_action_pressed("move_forward"):
-		direction -= transform.basis.z
-		
+func _input(event):
+	
+	if event is InputEventMouseMotion:
+		$Camera.rotation_degrees.x -= event.relative.y * sensitivity
+		$Camera.rotation_degrees.x = clamp($Camera.rotation_degrees.x, -90, 90)
+		rotation_degrees.y -= event.relative.x * sensitivity
+	
+
+func _physics_process(delta):
+	var look_direction = Vector3.ZERO
+	look_direction = Input.get_vector("look_left", "look_right", "look_up", "look_down")
+	rotate_y(look_direction.x * -sensitivity) 
+	$Camera.rotate_x(look_direction.y * -sensitivity) 
+	$Camera.rotation_degrees.x = clamp($Camera.rotation_degrees.x, -90, 90)
+	
 	if Input.is_action_pressed("respawn"):
 		position = Vector3(0.5,-3.5,10)
 
@@ -39,6 +37,10 @@ func _physics_process(delta):
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		target_velocity.y = jump_impulse
 		
+	var move_input_direction = Vector3.ZERO
+	move_input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction = (transform.basis * Vector3(move_input_direction.x, 0, move_input_direction.y)).normalized()
+	
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.basis = Basis.looking_at(direction)
